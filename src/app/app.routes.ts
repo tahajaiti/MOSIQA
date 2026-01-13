62e8d10b8b1a04d6af92f7efd751aa063ea611a5
tahajaiti <taha.jaiti@gmail.com>
2026-01-13 17:00:31 +0100

    FEAT: MQ-7 initialize routes with mainlayout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'library',
+        pathMatch: 'full',
+      },
+      {
+        path: 'library',
+        loadComponent: () => import('./features/library/library').then((m) => m.Library),
+      },
+    ],
+  },
+];
